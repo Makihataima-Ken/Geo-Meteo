56dea9a2a0a4c3da7cb4b24ad94acd4b80fb14d6
Ken <ahmadsoud63@gmail.com>
2025-04-13 16:59:10 +0300

    add geo function

---
+++ geo.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_coordinates(city_name):
+    url = "https://nominatim.openstreetmap.org/search" # OpenStreetMap (via Nominatim)
+    params = {"q": city_name, "format": "json"}
+    response = requests.get(url, params=params)
+    data = response.json()
+    if not data:
+        raise Exception("City not found")
+    lat = data[0]["lat"]
+    lon = data[0]["lon"]
+    display_name = data[0]["display_name"]
+    return lat, lon, display_name
